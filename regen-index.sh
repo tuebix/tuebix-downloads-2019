a2e5ad04f276ef3405bb8ae8c8399726202cc238
Michael Weiss <dev.primeos@gmail.com>
2019-07-08 14:00:59 +0200

    Add regen-index.sh and index.md for GitHub Pages
    This makes it easier to preview the slides with a webbrowser :)

---
+++ regen-index.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+cd $(dirname $0)
+
+echo -e "# Index\n" > index.md
+
+echo -e "## Folien\n" >> index.md
+
+find -type f ! -empty | xargs git ls-files -z | tr '\0' '\n' | \
+  grep -vE "README.md|index.md" | grep -E "\.(pdf|txt|md)$" | \
+  sed -E "s/tuebix-2019-(.*)/- [\1](tuebix-2019-\1)/" >> index.md
+
+echo -e "\n## Audio\n" >> index.md
+
+find -type f ! -empty | xargs git ls-files -z | tr '\0' '\n' | \
+  grep -E "\.ogg$" | sed -E "s/tuebix-2019-(.*)/- [\1](tuebix-2019-\1)/" >> index.md
