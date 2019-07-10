77861bc45e6b3110b12b0919c479adc8a755f98f
Christoph Prokop <christoph.prokop@mailbox.org>
2019-07-11 00:19:29 +0200

    index

--- regen-index.sh
+++ regen-index.sh
@@ -10,7 +10,7 @@ echo -e "# Index\n" > index.md
 echo -e "## Folien\n" >> index.md
 
 find -type f ! -empty | xargs git ls-files -z | tr '\0' '\n' | \
-  grep -vE "README.md|index.md" | grep -E "\.(pdf|txt|md)$" | \
+  grep -vE "README.md|index.md" | grep -E "\.(pdf|txt|md|cast|tar.gz)$" | \
   sed -E "s/tuebix-2019-(.*)/- [\1](tuebix-2019-\1)/" >> index.md
 
 echo -e "\n## Audio\n" >> index.md
